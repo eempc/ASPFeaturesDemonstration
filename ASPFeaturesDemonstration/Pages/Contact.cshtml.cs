980e5e9cefda6d841b5eecd7b07debd3c1599fd3
eempc <evc484@gmail.com>
2019-11-22 10:33:58 +0000

    working and deployed contact form

---
+++ ASPFeaturesDemonstration/Pages/Contact.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace ASPFeaturesDemonstration.Pages {
+    public class ContactModel : PageModel {
+        [BindProperty]
+        public ContactFormModel Contact { get; set; }
+
+        public string Message { get; set; }
+        
+
+        public async Task OnGetAsync() {
+            Message = "Fine";
+
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            string password = "";
+
+            try {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                SecretBundle secret = await keyVaultClient.GetSecretAsync("https://Key-Vault-11223344.vault.azure.net/secrets/GmailPassword").ConfigureAwait(false);
+                password = secret.Value;
+            } catch (KeyVaultErrorException e) {
+                Message = e.Message;
+            }
+
+            if (string.IsNullOrEmpty(password)) {
+                Message = "not fine";
+                return Page();
+            }
+
+            MailAddress fromAddress = new MailAddress("evctestemail@gmail.com", "No Reply");
+            MailAddress toAddress = new MailAddress("evc776@gmail.com");
+
+            SmtpClient smtp = new SmtpClient {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, password)
+            };
+
+            using MailMessage msg = new MailMessage(fromAddress, toAddress) {
+                Subject = $"Message from {Contact.Name} with subject: {Contact.Subject}",
+                Body = Contact.Message
+            };
+
+            await smtp.SendMailAsync(msg);
+            msg.Dispose();
+            smtp.Dispose();
+
+            return RedirectToPage("Index");
+        }
+
+    }
+
+    public class ContactFormModel {
+        [Required]
+        public string Name { get; set; }
+        [Required, DataType(DataType.EmailAddress), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+        public string Subject { get; set; }
+    }
+}
